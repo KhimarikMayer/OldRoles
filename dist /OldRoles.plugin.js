2ab26a3248e614556b7ad9d7e04c89a2d35f3bb6
KhimarikMayer <141296494+KhimarikMayer@users.noreply.github.com>
2026-04-14 09:57:13 +0900

    Update OldRoles.plugin.js

--- dist /OldRoles.plugin.js
+++ dist /OldRoles.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name OldRoles
  * @author KhimarikMayer
- * @description A full accurate restoration of Discord's roles layout
+ * @description A full accurate restoration of Discord's roles layout, which used from 2017 to 08.2021 (to 09.2022, because then the styles of border roles were still don't removed since 08.2021).
  * @version 1.0
  */
 
